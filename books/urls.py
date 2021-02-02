a2e2640c0f22f914e158c5e4f5cc01d9c14a3880
Фарида <happy_w@bk.ru>
2021-02-02 21:17:18 +0600

    add all

--- books/urls.py
+++ books/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from shop.views import books
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", books, name="books")
-]
+    path("", books, name="books"),
+]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
