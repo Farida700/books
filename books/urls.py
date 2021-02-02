c51e2d391206e3090bce00b38f30b6b00b1c1407
Фарида <happy_w@bk.ru>
2021-02-02 22:15:19 +0600

    added

--- books/urls.py
+++ books/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", books, name="books"),
+    path("add-todo/", add_todo, name"add_todo"),
 ]   +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Books
 
 # Create your views here.
@@ -7,3 +7,10 @@ from .models import Books
 def books(request):
     books_list = Books.objects.all()
     return render(request, "books.html", {"books_list": books_list})
+
+def add_todo(request):
+    form = request.POST
+    text = form["books_text"]
+    books = Books(text=text)
+    books.save()
+    return redirect(books)
