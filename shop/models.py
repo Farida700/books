5976802ad2ac44f055fe4c9c53595557984c10e1
Фарида <happy_w@bk.ru>
2021-02-03 12:10:56 +0600

    added new

--- shop/models.py
+++ shop/models.py
@@ -3,8 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Books(models.Model):
-    text = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=500)
+    description = models.CharField(max_length=1000)
+    price = models.CharField(max_length=10)
+    genre = models.CharField(max_length=20)
+    author = models.CharField(max_length=50)
+    year = models.DateField(auto_created=True)
+    date = models.DateField(auto_now_add=True)
+    is_favorite = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+    
     
--- shop/views.py
+++ shop/views.py
@@ -11,41 +11,41 @@ def books(request):
 
 def add_books(request):
     form = request.POST
-    title = request.POST['book_title']
-    subtitle = request.POST['book_subtitle']
-    description = request.POST['book_decscription']
-    price = request.POST['book_price']
-    genre = request.POST['book_genre']
-    author = request.POST['book_author']
-    year = request.POST['book_year']
+    title = form['book_title']
+    subtitle = form['book_subtitle']
+    description = form['book_decscription']
+    price = form['book_price']
+    genre = form['book_genre']
+    author = form['book_author']
+    year = form['book_year']
     books = Books(title=title, subtitle=subtitle, description=description, price=price, genre=genre, author=author, year=year)
     books.save()
     return redirect(books)
 
 def delete_books(request, id):
-    todo = Book.objects.get(id=id)
-    todo.delete()
+    books = Book.objects.get(id=id)
+    books.delete()
     return redirect(books)
 
 def unmark_books(request, id):
-    todo = Book.objects.get(id=id)
-    books.is_favorite = True
-    todo.save()
+    books = Book.objects.get(id=id)
+    books.is_favorite = False
+    books.save()
     return redirect(books)
 
 def mark_books(request, id):
-    todo = Book.objects.get(id=id)
-    books.is_favorite = False
-    todo.save()
+    books = Book.objects.get(id=id)
+    books.is_favorite = True
+    books.save()
     return redirect(books)
 
 def close_books(request, id):
-    todo = Book.objects.get(id=id)
+    books = Book.objects.get(id=id)
     books.is_closed = not books.is_closed
-    todo.save()
+    books.save()
     return redirect(books)
 
 def books_detail(request, id):
-    books_list=Book.objects.get(id=id)
-    return render(request, "books_detail.html", {"books_list": books_list})
+    books = Book.objects.filter(id=id)
+    return render(request, "books_detail.html", {"books_list": mybook})
 
